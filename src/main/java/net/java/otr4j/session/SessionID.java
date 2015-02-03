f6cf71aba5d401d08adfb92d94536a52fd1cf53c
Hans-Christoph Steiner <hans@eds.org>
2015-02-03 11:19:58 +0100

    document the two Strings representing the sides of the convo in SessionID
    The terms used here are confusing since both sides of the conversation have
    accounts and users, so this documents things to clarify the distinction,
    but leaves the API the same.

--- src/main/java/net/java/otr4j/session/SessionID.java
+++ src/main/java/net/java/otr4j/session/SessionID.java
@@ -4,81 +4,95 @@
  * Distributable under LGPL license.
  * See terms of license at gnu.org.
  */
+
 package net.java.otr4j.session;
 
 /**
- * 
  * @author George Politis
- * 
  */
 public final class SessionID {
 
-	public SessionID(String accountID, String userID, String protocolName) {
-		this.accountID = accountID;
-		this.userID = userID;
-		this.protocolName = protocolName;
-	}
+    /**
+     * A unique ID for an OTR session between two accounts.
+     *
+     * @param localAccountID the local account used for this conversation
+     * @param remoteUserID the remote user on the other side of the conversation
+     * @param protocolName the messaging protocol used for the conversation
+     */
+    public SessionID(String localAccountID, String remoteUserID, String protocolName) {
+        this.localAccountID = localAccountID;
+        this.remoteUserID = remoteUserID;
+        this.protocolName = protocolName;
+    }
 
-	private final String accountID;
-	private final String userID;
-	private final String protocolName;
+    private final String localAccountID;
+    private final String remoteUserID;
+    private final String protocolName;
 
-	public static final SessionID Empty = new SessionID(null, null, null);
+    public static final SessionID Empty = new SessionID(null, null, null);
 
-	public String getAccountID() {
-		return accountID;
-	}
+    /**
+     *
+     * @return the {@code String} representing the local account
+     */
+    public String getAccountID() {
+        return localAccountID;
+    }
 
-	public String getUserID() {
-		return userID;
-	}
+    /**
+     *
+     * @return the {@code String} representing the remote user
+     */
+    public String getUserID() {
+        return remoteUserID;
+    }
 
-	public String getProtocolName() {
-		return protocolName;
-	}
+    public String getProtocolName() {
+        return protocolName;
+    }
 
-	@Override
-	public String toString() {
-		return accountID + '_' + protocolName + '_' + userID;
-	}
+    @Override
+    public String toString() {
+        return localAccountID + '_' + protocolName + '_' + remoteUserID;
+    }
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result
-				+ ((accountID == null) ? 0 : accountID.hashCode());
-		result = prime * result
-				+ ((protocolName == null) ? 0 : protocolName.hashCode());
-		result = prime * result + ((userID == null) ? 0 : userID.hashCode());
-		return result;
-	}
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result
+                + ((localAccountID == null) ? 0 : localAccountID.hashCode());
+        result = prime * result
+                + ((protocolName == null) ? 0 : protocolName.hashCode());
+        result = prime * result + ((remoteUserID == null) ? 0 : remoteUserID.hashCode());
+        return result;
+    }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		SessionID other = (SessionID) obj;
-		if (accountID == null) {
-			if (other.accountID != null)
-				return false;
-		} else if (!accountID.equals(other.accountID))
-			return false;
-		if (protocolName == null) {
-			if (other.protocolName != null)
-				return false;
-		} else if (!protocolName.equals(other.protocolName))
-			return false;
-		if (userID == null) {
-			if (other.userID != null)
-				return false;
-		} else if (!userID.equals(other.userID))
-			return false;
-		return true;
-	}
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        SessionID other = (SessionID) obj;
+        if (localAccountID == null) {
+            if (other.localAccountID != null)
+                return false;
+        } else if (!localAccountID.equals(other.localAccountID))
+            return false;
+        if (protocolName == null) {
+            if (other.protocolName != null)
+                return false;
+        } else if (!protocolName.equals(other.protocolName))
+            return false;
+        if (remoteUserID == null) {
+            if (other.remoteUserID != null)
+                return false;
+        } else if (!remoteUserID.equals(other.remoteUserID))
+            return false;
+        return true;
+    }
 
 }
