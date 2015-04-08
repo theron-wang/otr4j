9b9174786c533392ee1625b6e8aeae86eb8f18da
Hans-Christoph Steiner <hans@eds.org>
2015-04-07 21:02:59 -0400

    move unicode test strings to separate file for easy reuse
    Also, the line spacing gets extra big when there are so many different
    kinds of text in a single file, so keeping the unicode strings in a single
    file keeps the test java files easy to read.  At least with Eclipse...

---
+++ src/test/java/net/java/otr4j/test/TestStrings.java
@@ -0,0 +1,34 @@
+
+package net.java.otr4j.test;
+
+public class TestStrings {
+
+    // TODO these currently cause an Exception when used as SMP question:
+    // "nullshere:\0\0andhere:\0",
+    // "tabbackslashT\t",
+    // "backslashR\r",
+    // "NEWLINE\n",
+    public static String[] unicodes = {
+            "plainAscii",
+            "",
+            "བོད་རིགས་ཀྱི་བོད་སྐད་བརྗོད་པ་དང་ བོད་རིགས་མང་ཆེ་བ་ནི་ནང་ཆོས་བྱེད་པ་དང་",
+            "تبتی قوم (Tibetan people)",
+            "Учените твърдят, че тибетците нямат",
+            "Câung-cŭk (藏族, Câung-ngṳ̄: བོད་པ་)",
+            "チベット系民族（チベットけいみんぞく）",
+            "原始汉人与原始藏缅人约在公元前4000年左右分开。",
+            "Տիբեթացիներ (ինքնանվանումը՝ պյոբա),",
+            "... Gezginci olarak",
+            "شْتَن Xotan",
+            "Tibeťané jsou",
+            "ئاچاڭ- تىبەت مىللىتى",
+            "Miscellaneous Symbols and Pictographs[1][2] Official Unicode Consortium code chart (PDF)",
+            "Royal Thai (ราชาศัพท์)",
+            "טיילאנדיש123 (ภาษาไทย)",
+            "ជើងអក្សរ cheung âksâr",
+            "중화인민공화국에서는 기본적으로 한족은 ",
+            "पाठ्यांशः अत्र उपलभ्यतेसर्जनसामान्यलक्षणम्/Share-",
+            "திபெத்துக்கு வெகள்",
+            "អក្សរសាស្រ្តខែ្មរមានប្រវ៌ត្តជាងពីរពាន់ឆ្នាំមកហើយ ",
+    };
+}
