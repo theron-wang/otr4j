c59c31dc32555f157a79c451fcfa7b99ff1ee227
Danny van Heumen <danny@dannyvanheumen.nl>
2016-01-13 23:26:28 +0100

    Test for successful SMP conversation.

--- src/test/java/net/java/otr4j/crypto/SMTest.java
+++ src/test/java/net/java/otr4j/crypto/SMTest.java
@@ -1,7 +1,16 @@
 package net.java.otr4j.crypto;
 
+import java.io.ByteArrayOutputStream;
 import java.math.BigInteger;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
 import java.security.SecureRandom;
+import java.security.Security;
+import net.java.otr4j.io.OtrOutputStream;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
@@ -13,6 +22,10 @@ import org.junit.Test;
  * @author Danny van Heumen
  */
 public class SMTest {
+    
+    static {
+        Security.addProvider(new BouncyCastleProvider());
+    }
 
     @Test
     public void testCheckGroupElemValid() throws SM.SMException {
@@ -118,5 +131,73 @@ public class SMTest {
     @SuppressWarnings("ResultOfObjectAllocationIgnored")
     public void testConstructionWithValidSecureRandom() {
         new SM(new SecureRandom());
+    }
+
+    @Test
+    public void testSuccessfulSMPConversation() throws SM.SMException, OtrCryptoException, Exception {
+        final SecureRandom rand = new SecureRandom();
+
+        // Alice
+        final SM.SMState alice = new SM.SMState();
+        final KeyPair aliceKeyPair = generateKeyPair();
+        final KeyPair aliceDHKeyPair = OtrCryptoEngine.generateDHKeyPair(rand);
+        final byte[] alicePublic = OtrCryptoEngine.getFingerprintRaw(aliceKeyPair.getPublic());
+
+        // Bob
+        final SM.SMState bob = new SM.SMState();
+        final KeyPair bobKeyPair = generateKeyPair();
+        final KeyPair bobDHKeyPair = OtrCryptoEngine.generateDHKeyPair(rand);
+        final byte[] bobPublic = OtrCryptoEngine.getFingerprintRaw(bobKeyPair.getPublic());
+
+        // Shared secret
+        final byte[] secret = new byte[] { 'p', 'a', 's', 's', 'w', 'o', 'r', 'd' };
+        final BigInteger s = OtrCryptoEngine.generateSecret(aliceDHKeyPair.getPrivate(), bobDHKeyPair.getPublic());
+        final byte[] combinedSecretBytes = combinedSecret(alicePublic, bobPublic, s, secret);
+
+        // SMP session execution
+        final SM sm = new SM(rand);
+        final byte[] msg1 = sm.step1(alice, combinedSecretBytes);
+        sm.step2a(bob, msg1, 0);
+        final byte[] msg2 = sm.step2b(bob, combinedSecretBytes);
+        final byte[] msg3 = sm.step3(alice, msg2);
+        final byte[] msg4 = sm.step4(bob, msg3);
+        sm.step5(alice, msg4);
+
+        // Evaluate session result
+        assertEquals(alice.smProgState, SM.PROG_SUCCEEDED);
+        assertEquals(bob.smProgState, SM.PROG_SUCCEEDED);
+    }
+
+    private byte[] combinedSecret(final byte[] alicePublic, final byte[] bobPublic, final BigInteger s, final byte[] secret) throws Exception {
+        final byte[] sessionBytes = computeSessionId(s);
+        final byte[] combinedSecret = new byte[1 + alicePublic.length + bobPublic.length + sessionBytes.length + secret.length];
+        combinedSecret[0] = 1;
+        System.arraycopy(alicePublic, 0, combinedSecret, 1, alicePublic.length);
+        System.arraycopy(bobPublic, 0, combinedSecret, 1+alicePublic.length, bobPublic.length);
+        System.arraycopy(sessionBytes, 0, combinedSecret, 1+alicePublic.length+bobPublic.length, sessionBytes.length);
+        System.arraycopy(secret, 0, combinedSecret, 1+alicePublic.length+bobPublic.length+sessionBytes.length, secret.length);
+        return combinedSecret;
+    }
+
+	/* Compute secret session ID as hash of agreed secret */
+	private static byte[] computeSessionId(final BigInteger s) throws Exception {
+        final ByteArrayOutputStream out = new ByteArrayOutputStream();
+        final OtrOutputStream oos = new OtrOutputStream(out);
+        oos.write(0x00);
+        oos.writeBigInt(s);
+        final byte[] sdata = out.toByteArray();
+        oos.close();
+
+		/* Calculate the session id */
+		final MessageDigest sha256 = MessageDigest.getInstance("SHA-256");
+		final byte[] res = sha256.digest(sdata);
+		final byte[] secure_session_id = new byte[8];
+		System.arraycopy(res, 0, secure_session_id, 0, 8);
+		return secure_session_id;
+	}
+
+    private static KeyPair generateKeyPair() throws NoSuchAlgorithmException, NoSuchProviderException {
+        final KeyPairGenerator kg = KeyPairGenerator.getInstance("DSA", "BC");
+        return kg.genKeyPair();
     }
 }
