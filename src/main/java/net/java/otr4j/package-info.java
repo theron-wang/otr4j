b61766f125d21153c4bd59cbc720c14e6b74164f
Danny van Heumen <danny@dannyvanheumen.nl>
2016-01-15 21:38:48 +0100

    Added package-info. Includes package-wide TODO item.

---
+++ src/main/java/net/java/otr4j/package-info.java
@@ -0,0 +1,11 @@
+/*
+ * otr4j, the open source java otr library.
+ *
+ * Distributable under LGPL license.
+ * See terms of license at gnu.org.
+ */
+// TODO verify that all (new) files have correct LICENSE header
+/**
+ * otr4j.
+ */
+package net.java.otr4j;
