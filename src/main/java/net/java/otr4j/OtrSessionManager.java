39a2ec6d2e5b7512fee1c9f4b9964b1ece2df044
Danny van Heumen <danny@dannyvanheumen.nl>
2016-01-15 20:56:26 +0100

    Comments, replaced Vector with ArrayList, notes for future TODOs.

--- src/main/java/net/java/otr4j/OtrSessionManager.java
+++ src/main/java/net/java/otr4j/OtrSessionManager.java
@@ -7,16 +7,16 @@
 
 package net.java.otr4j;
 
+import java.util.ArrayList;
 import java.util.Hashtable;
-import java.util.List;
 import java.util.Map;
-import java.util.Vector;
 
 import net.java.otr4j.session.Session;
 import net.java.otr4j.session.SessionID;
 
 /**
  * @author George Politis
+ * @author Danny van Heumen
  */
 public class OtrSessionManager {
 
@@ -28,15 +28,28 @@ public class OtrSessionManager {
         this.host = host;
     }
 
+    /**
+     * The OTR Engine Host instance.
+     */
     private final OtrEngineHost host;
+
+    /**
+     * Map with known sessions.
+     */
+    // TODO consider creating a map on construction and making it final. Map could be minimum-sized to avoid large overhead in memory usage.
     private Map<SessionID, Session> sessions;
+
+    /**
+     * List for keeping track of listeners.
+     */
+    private final ArrayList<OtrEngineListener> listeners = new ArrayList<OtrEngineListener>(0);
 
     /**
      * Fetches the existing session with this {@link SessionID} or creates a new
      * {@link Session} if one does not exist.
      *
      * @param sessionID
-     * @return MVN_PASS_JAVADOC_INSPECTION
+     * @return Returns Session instance that corresponds to provided sessionID.
      */
     public Session getSession(final SessionID sessionID) {
 
@@ -53,20 +66,24 @@ public class OtrSessionManager {
             final Session session = new Session(sessionID, this.host);
             sessions.put(sessionID, session);
 
+            // TODO consider refactoring this to singleton instance of an OtrEngineListener that gets re-used for each new sessions. This should be feasible, as the listener only reads data and does not modify state.
             session.addOtrEngineListener(new OtrEngineListener() {
 
                 @Override
                 public void sessionStatusChanged(final SessionID sessionID) {
+                    // FIXME consider copying 'listeners' to avoid incidental ConcurrentModificationExceptions while iterating over elements (thread-safety)
                     OtrEngineListenerUtil.sessionStatusChanged(listeners, sessionID);
                 }
 
                 @Override
                 public void multipleInstancesDetected(final SessionID sessionID) {
+                    // FIXME consider copying 'listeners' to avoid incidental ConcurrentModificationExceptions while iterating over elements (thread-safety)
                     OtrEngineListenerUtil.multipleInstancesDetected(listeners, sessionID);
                 }
 
                 @Override
                 public void outgoingSessionChanged(final SessionID sessionID) {
+                    // FIXME consider copying 'listeners' to avoid incidental ConcurrentModificationExceptions while iterating over elements (thread-safety)
                     OtrEngineListenerUtil.outgoingSessionChanged(listeners, sessionID);
                 }
             });
@@ -76,9 +93,11 @@ public class OtrSessionManager {
         }
     }
 
-    // TODO consider converting Vector to ArrayList (check synchronization)
-    private final List<OtrEngineListener> listeners = new Vector<OtrEngineListener>();
-
+    /**
+     * Add a new OtrEngineListener.
+     *
+     * @param l the listener
+     */
     public void addOtrEngineListener(final OtrEngineListener l) {
         synchronized (listeners) {
             if (!listeners.contains(l)) {
@@ -87,6 +106,11 @@ public class OtrSessionManager {
         }
     }
 
+    /**
+     * Remove a registered OtrEngineListener.
+     *
+     * @param l the listener
+     */
     public void removeOtrEngineListener(final OtrEngineListener l) {
         synchronized (listeners) {
             listeners.remove(l);
