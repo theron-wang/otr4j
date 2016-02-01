4662fe751372c971d28973250db3700df2a2410c
Danny van Heumen <danny@dannyvanheumen.nl>
2016-02-01 19:18:55 +0100

    Work-in-progress ...

--- src/main/java/net/java/otr4j/package-info.java
+++ src/main/java/net/java/otr4j/package-info.java
@@ -7,6 +7,7 @@
 // TODO consider making classes final where obvious (OtrInputStream, OtrOutputStream, SM, ...)
 // TODO remove traces of OTRv1 protocol, as this should not be supported anymore.
 // FIXME don't forget to include license header in all new test-files too!
+// TODO Outstanding issue: In several places arrays exposed as public fields/through accessor methods which allows code to modify its contents. (Fixed for crypto constants, not for AES keys + MAC keys + CTR values, TLV values, etc.) (Detected by FindBugs.)
 /**
  * otr4j.
  */
