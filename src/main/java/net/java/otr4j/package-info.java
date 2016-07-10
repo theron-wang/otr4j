446912c04bfe990dda122828d319f0df901b9b7a
Danny van Heumen <danny@dannyvanheumen.nl>
2016-07-10 20:00:48 +0200

    Added license header to some files.

--- src/main/java/net/java/otr4j/package-info.java
+++ src/main/java/net/java/otr4j/package-info.java
@@ -5,7 +5,6 @@
  * See terms of license at gnu.org.
  */
 // TODO remove traces of OTRv1 protocol, as this should not be supported anymore.
-// FIXME don't forget to include license header in all new test-files too!
 // TODO Outstanding issue: In several places arrays exposed as public fields/through accessor methods which allows code to modify its contents. (Fixed for crypto constants, not for AES keys + MAC keys + CTR values, TLV values, etc.) (Detected by FindBugs.)
 /**
  * otr4j.
--- src/test/java/net/java/otr4j/crypto/OtrCryptoEngineTest.java
+++ src/test/java/net/java/otr4j/crypto/OtrCryptoEngineTest.java
@@ -1,3 +1,10 @@
+/*
+ * otr4j, the open source java otr library.
+ *
+ * Distributable under LGPL license.
+ * See terms of license at gnu.org.
+ */
+
 package net.java.otr4j.crypto;
 
 import java.security.KeyPair;
@@ -12,12 +19,12 @@ import static org.junit.Assert.*;
  */
 public class OtrCryptoEngineTest {
 
-    private static final SecureRandom rand = new SecureRandom();
+    private static final SecureRandom RAND = new SecureRandom();
 
     @Test
     public void testGeneratedSharedSecretEqual() throws OtrCryptoException {
-        final KeyPair aliceDHKeyPair = OtrCryptoEngine.generateDHKeyPair(rand);
-        final KeyPair bobDHKeyPair = OtrCryptoEngine.generateDHKeyPair(rand);
+        final KeyPair aliceDHKeyPair = OtrCryptoEngine.generateDHKeyPair(RAND);
+        final KeyPair bobDHKeyPair = OtrCryptoEngine.generateDHKeyPair(RAND);
 
         assertEquals(OtrCryptoEngine.generateSecret(aliceDHKeyPair.getPrivate(), bobDHKeyPair.getPublic()),
                 OtrCryptoEngine.generateSecret(bobDHKeyPair.getPrivate(), aliceDHKeyPair.getPublic()));
