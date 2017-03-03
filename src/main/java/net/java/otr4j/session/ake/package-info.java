28ef2167182a2708086b4216665b76d22619a237
Danny van Heumen <danny@dannyvanheumen.nl>
2017-03-04 00:27:13 +0100

    Don't try to eradicate use of InstanceTag, instead define domain package that contains these "primitives".

--- src/main/java/net/java/otr4j/session/ake/package-info.java
+++ src/main/java/net/java/otr4j/session/ake/package-info.java
@@ -5,6 +5,5 @@
  * JVM is lacking, such as for required hashing functions and ciphers.
  */
 // TODO need/required to clear temporary bytearray data containing key information? (AKE states)
-// TODO consider not using InstanceTag type in this package and instead sticking with int values ... To improve reusability as stand-alone package.
 // TODO should all states except for Initial (AUTHSTATE_NONE) have some kind of inherent time-out such that we cannot infinitely "stay" on a single state? (e.g. AWAITING_DHKEY in case of DH-Commit w/o receiver tag)
 package net.java.otr4j.session.ake;
