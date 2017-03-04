cf17eb3355f95362f8f5e20d3894b563247a9376
Danny van Heumen <danny@dannyvanheumen.nl>
2017-03-04 19:48:39 +0100

    Reduce access level of DEFAULT_REPLY_UNREADABLE_MESSAGE.

--- src/main/java/net/java/otr4j/session/state/AbstractState.java
+++ src/main/java/net/java/otr4j/session/state/AbstractState.java
@@ -19,7 +19,7 @@ import net.java.otr4j.io.messages.ErrorMessage;
  */
 abstract class AbstractState implements State {
 
-    protected static final String DEFAULT_REPLY_UNREADABLE_MESSAGE = "This message cannot be read.";
+    static final String DEFAULT_REPLY_UNREADABLE_MESSAGE = "This message cannot be read.";
 
     @Override
     public void handleErrorMessage(@Nonnull final Context context, @Nonnull final ErrorMessage errorMessage) throws OtrException {
