8a4af146ff26e2b1eefd242b2b8aa989fe782473
Danny van Heumen <danny@dannyvanheumen.nl>
2018-03-07 23:15:30 +0100

    Added test for creating and parsing DHKey message.

--- src/test/java/net/java/otr4j/io/messages/EncodedMessageParserTest.java
+++ src/test/java/net/java/otr4j/io/messages/EncodedMessageParserTest.java
@@ -1,17 +1,27 @@
 package net.java.otr4j.io.messages;
 
+import net.java.otr4j.api.Session;
+import net.java.otr4j.crypto.OtrCryptoEngine;
 import net.java.otr4j.crypto.OtrCryptoException;
 import net.java.otr4j.io.OtrInputStream;
+import net.java.otr4j.io.OtrOutputStream;
 import org.junit.Test;
 
+import javax.crypto.interfaces.DHPublicKey;
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.net.ProtocolException;
+import java.security.KeyPair;
+import java.security.SecureRandom;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
-// TODO Need to add tests for parsing various type of encoded messages, instead of only testing exception cases.
+// TODO Need to add tests for parsing various type of encoded messages.
 public class EncodedMessageParserTest {
+
+    private static final SecureRandom RANDOM = new SecureRandom();
 
     @Test
     public void testInstanceAvailable() {
@@ -31,5 +41,20 @@ public class EncodedMessageParserTest {
     @Test(expected = ProtocolException.class)
     public void testParsingIncompleteInputStream() throws IOException, OtrCryptoException {
         EncodedMessageParser.instance().read(new OtrInputStream(new ByteArrayInputStream(new byte[] { 0x00, 0x03 })));
+    }
+
+    @Test
+    public void testConstructAndParseDHKeyMessage() throws IOException, OtrCryptoException {
+        final KeyPair keypair = OtrCryptoEngine.generateDHKeyPair(RANDOM);
+        // Prepare output message to parse.
+        final DHKeyMessage m = new DHKeyMessage(Session.OTRv.THREE, (DHPublicKey) keypair.getPublic(), 12345, 9876543);
+        final ByteArrayOutputStream output = new ByteArrayOutputStream();
+        final OtrOutputStream otrOutput = new OtrOutputStream(output);
+        m.write(otrOutput);
+        // Parse produced message bytes.
+        final ByteArrayInputStream input = new ByteArrayInputStream(output.toByteArray());
+        final OtrInputStream otrInput = new OtrInputStream(input);
+        final AbstractEncodedMessage parsedM = EncodedMessageParser.instance().read(otrInput);
+        assertEquals(m, parsedM);
     }
 }
