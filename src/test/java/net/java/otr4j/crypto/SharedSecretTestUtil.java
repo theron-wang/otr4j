e8dbbbf7af74e26605fa970c792ab9de3a2aef33
Danny van Heumen <danny@dannyvanheumen.nl>
2018-05-13 21:30:43 +0200

    Added private constructor to utility class.

--- src/test/java/net/java/otr4j/crypto/SharedSecretTestUtil.java
+++ src/test/java/net/java/otr4j/crypto/SharedSecretTestUtil.java
@@ -6,6 +6,10 @@ public class SharedSecretTestUtil {
 
     private static final SecureRandom RANDOM = new SecureRandom();
 
+    private SharedSecretTestUtil() {
+        // No need to instantiate utility class.
+    }
+
     public static SharedSecret createTestSecret() {
         return new SharedSecret(OtrCryptoEngine.random(RANDOM, new byte[24]));
     }
