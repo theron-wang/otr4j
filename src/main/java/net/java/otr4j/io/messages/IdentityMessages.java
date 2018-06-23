9ace545dae009afd86daa8524e8bbf38a2d2b7a8
Danny van Heumen <danny@dannyvanheumen.nl>
2018-06-23 16:54:35 +0200

    Finished validation of Identity messages.

--- src/main/java/net/java/otr4j/io/messages/IdentityMessages.java
+++ src/main/java/net/java/otr4j/io/messages/IdentityMessages.java
@@ -8,12 +8,22 @@ import javax.annotation.Nonnull;
 import static net.java.otr4j.crypto.DHKeyPairs.verifyDHPublicKey;
 import static net.java.otr4j.crypto.ECDHKeyPairs.verifyECDHPublicKey;
 
+/**
+ * Utilities for identity messages.
+ */
 public final class IdentityMessages {
 
     private IdentityMessages() {
         // No need to instantiate utility class.
     }
 
+    /**
+     * Validate identity message.
+     *
+     * @param message The identity message.
+     * @throws OtrCryptoException                       Validation failure of cryptographic components.
+     * @throws ClientProfilePayload.ValidationException Validation failure of client profile.
+     */
     public static void validate(@Nonnull final IdentityMessage message) throws OtrCryptoException, ClientProfilePayload.ValidationException {
 
         if (message.getType() != IdentityMessage.MESSAGE_IDENTITY) {
@@ -22,11 +32,9 @@ public final class IdentityMessages {
         if (message.protocolVersion != Session.OTRv.FOUR) {
             throw new IllegalStateException("Identity message should not have any other protocol version than 4.");
         }
-        // FIXME consider moving this out to first use case, instead of prematurely validating here.
+        // TODO consider moving this out to first use case, instead of prematurely validating here.
         message.getClientProfile().validate();
         verifyECDHPublicKey(message.getY());
         verifyDHPublicKey(message.getB());
-        // FIXME finish implementation.
-        throw new UnsupportedOperationException("To be implemented.");
     }
 }
