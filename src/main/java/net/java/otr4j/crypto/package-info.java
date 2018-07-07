0836f7cfc543d0e9dc826a8216d13a95eac49ea1
Danny van Heumen <danny@dannyvanheumen.nl>
2018-07-07 16:36:18 +0200

    Update BC to 1.60. Add FIXME for migrating to BC's Ed448-Goldilocks.

--- src/main/java/net/java/otr4j/crypto/package-info.java
+++ src/main/java/net/java/otr4j/crypto/package-info.java
@@ -8,4 +8,5 @@
  * Package containing cryptographic support logic for otr4j.
  */
 // TODO ensure that StateEncrypted4, MessageKeys, DoubleRatchet, SharedSecret4 are cleared after use.
+// FIXME BC 1.60 provides Ed448-Goldilocks, consider migrating away and using that implementation.
 package net.java.otr4j.crypto;
