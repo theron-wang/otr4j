4387a05d54722c518d63e8a23f001db8c38c99c3
Danny van Heumen <danny@dannyvanheumen.nl>
2019-08-20 21:30:02 +0200

    Added package-info file for otrfuzz package.

---
+++ src/test/java/net/java/otrfuzz/package-info.java
@@ -0,0 +1,12 @@
+/*
+ * otr4j, the open source java otr library.
+ *
+ * Distributable under LGPL license.
+ * See terms of license at gnu.org.
+ *
+ * SPDX-License-Identifier: LGPL-3.0-only
+ */
+/**
+ * Package containing fuzzer drivers for the JQF + afl framework.
+ */
+package net.java.otrfuzz;
